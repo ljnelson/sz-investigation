062a50f1cc41f0d2d5d50d45226d454e85530370
Laird Nelson <ljnelson@gmail.com>
2019-11-25 12:18:09 -0800

    Initial revision
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

---
+++ src/main/java/io/helidon/sz/investigation/ProbeResource.java
@@ -0,0 +1,54 @@
+package io.helidon.sz.investigation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.Objects;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.sql.DataSource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/probe")
+@ApplicationScoped
+public class ProbeResource {
+
+    private final DataSource dataSource;
+  
+    @Inject
+    public ProbeResource(@Named("probe") final DataSource dataSource) {
+        super();
+        this.dataSource = Objects.requireNonNull(dataSource);
+    }
+
+    @GET
+    @Path("")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String probe() {
+        String returnValue = null;
+        try (final Connection connection = this.dataSource.getConnection();
+             final Statement statement = connection.createStatement();
+             final ResultSet resultSet = statement.executeQuery("SELECT COUNT(TABLE_NAME) FROM INFORMATION_SCHEMA.TABLES")) {
+            resultSet.next();
+            final int count = resultSet.getInt(1);
+            if (count > 0) {
+                returnValue = "healthy";
+            } else {
+                returnValue = "unhealthy";
+            }
+        } catch (final SQLException kaboom) {
+            kaboom.printStackTrace();
+            returnValue = "unhealthy";
+        }
+        return returnValue;
+    }
+
+}
